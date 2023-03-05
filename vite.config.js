9c128748ee8d09c7ac4cdd27defcaad87a8418e0
Lawrence <e0176571@u.nus.edu>
2023-03-05 22:09:56 +0800

    update vite config path, remove jsconfig

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './resources/js'),
+            '@': path.resolve('resources/js'),
         },
     },
 });
