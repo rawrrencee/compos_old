f8537d09a7f0434d862a3b8a9299fb0b25a8d2fd
Lawrence <e0176571@u.nus.edu>
2023-03-24 01:12:39 +0800

    update application name

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import 'primeicons/primeicons.css';
 import 'primevue/resources/primevue.min.css';
-import 'primevue/resources/themes/fluent-light/theme.css';
+import 'primevue/resources/themes/lara-light-blue/theme.css';
 import '../css/app.css';
 import './bootstrap';
 
@@ -10,26 +10,21 @@ import PrimeVue from 'primevue/config';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const appName =
-    window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Compos';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue')
-        ),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(PrimeVue)
-            .use(ZiggyVue)
-            .mount(el);
-    },
-    progress: {
-        delay: 250,
-        color: '#4B5563',
-        includeCSS: true,
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+  setup({ el, App, props, plugin }) {
+    return createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(PrimeVue)
+      .use(ZiggyVue)
+      .mount(el);
+  },
+  progress: {
+    delay: 250,
+    color: '#4B5563',
+    includeCSS: true,
+  },
 });
